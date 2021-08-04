c279e17dab9e3d67877009ffc9f3d177738430b5
elena perez <ejperezk@gmail.com>
2021-08-04 17:44:53 +0200

    created function to sort stories in order of votes from highest to lowest

--- scrape.py
+++ scrape.py
@@ -9,6 +9,10 @@ links = soup.select('.storylink')
 subtext = soup.select('.subtext')
 
 
+def sort_stories_by_votes(hn_list):
+    return sorted(hn_list, key=lambda k: k['votes'], reverse=True)
+
+
 def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
@@ -17,8 +21,9 @@ def create_custom_hn(links, subtext):
         vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
-        hn.append({'title': title, 'link': href, 'votes': points})
-    return hn
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
 
 
 pprint.pprint(create_custom_hn(links, subtext))
